f90918d20db4f0d25007d05ed47825e0915fd7a0
Nima Akbarzade <Nimakbarzade@yahoo.com>
2023-04-14 21:15:46 +0200

    feat: add base tmpl

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -13,13 +13,17 @@ func home(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
-	ts ,err := template.ParseFiles("./ui/html/pages/home.tmpl")
+	files := []string{
+		"./ui/html/base.tmpl",
+		"./ui/html/pages/home.tmpl",
+	}
+	ts ,err := template.ParseFiles(files...)
 	if err != nil {
 		log.Println(err.Error())
 		http.Error(w, "Internal Server Error", 500)
 		return
 	}
-	err = ts.Execute(w, nil)
+	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
 		log.Println(err.Error())
 		http.Error(w, "Internal Server Error", 500)
