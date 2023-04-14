b64cb647605f0594c767e1a87a1dd5394eef5604
Nima Akbarzade <Nimakbarzade@yahoo.com>
2023-04-14 15:14:53 +0200

    add: id as a query param to get and show

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -14,7 +16,13 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func codeView(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a specific code"))
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "Display a specific snippet with ID %d", id)
+	// w.Write([]byte("Display a specific code"))
 }
 
 func codeCreate(w http.ResponseWriter, r *http.Request) {
