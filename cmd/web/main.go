2ac2c096b3e9c088c7ce7832827190262acc9385
Nima Akbarzade <Nimakbarzade@yahoo.com>
2023-04-14 21:35:38 +0200

    feat: css

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,6 +7,8 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+	fileServer := http.FileServer(http.Dir("./ui/static"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/code/view", codeView)
 	mux.HandleFunc("/code/create", codeCreate)
